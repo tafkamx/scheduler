8bcf44288862767efa29450b63e47541fc5156cf
greduan <me@greduan.com>
2016-03-23 12:53:25 -0600

    Fix code styles in InstallationsController

--- controllers/InstallationManager/InstallationsController.js
+++ controllers/InstallationManager/InstallationsController.js
@@ -7,96 +7,111 @@ InstallationManager.InstallationsController = Class(InstallationManager, 'Instal
     {
       before : [neonode.controllers.Home._authenticate],
       actions : ['index', 'show', 'create', 'edit', 'update', 'destroy']
-
     },
     {
-      before : ['_loadResource'],
+      before : ['_loadInstallation'],
       actions : ['show', 'edit', 'update', 'destroy']
     }
   ],
 
-  prototype : {
-    _loadResource : function(req, res, next) {
-      InstallationManager.Installation.query().where({id : req.params.id}).then(function(result) {
-
-        if (result.length === 0) {
-          throw new NotFoundError('Installation ' + req.params.id + ' not found');
-        }
-
-        req.installation = result[0];
-        next();
-      }).catch(function(err) {
-        next(err)
-      });
-    },
-
-    index : function index(req, res, next) {
-      InstallationManager.Installation.query().then(function(results) {
-        res.format({
-          html : function() {
-            res.render('InstallationManager/Installations/index.html', {installations : results});
-          },
-          json : function() {
-            res.json(results);
+  prototype: {
+    _loadInstallation: function (req, res, next) {
+      InstallationManager.Installation.query()
+        .where('id', req.params.id)
+        .then(function(result) {
+          if (result.length === 0) {
+            throw new NotFoundError('Installation ' + req.params.id + ' not found');
           }
-        });
-      }).catch(next);
+
+          req.installation = result[0];
+          res.locals.installation = result[0];
+
+          next();
+        })
+        .catch(next);
     },
 
-    show : function show(req, res, next) {
+    index: function (req, res, next) {
+      InstallationManager.Installation.query()
+        .then(function(results) {
+          res.locals.installations = results;
+
+          res.format({
+            html: function () {
+              res.render('InstallationManager/Installations/index.html');
+            },
+            json: function () {
+              res.json(results);
+            }
+          });
+        })
+        .catch(next);
+    },
+
+    show: function (req, res, next) {
       res.format({
-        html : function() {
-          res.render('InstallationManager/Installations/show.html', { installation : req.installation});
+        html: function () {
+          res.render('InstallationManager/Installations/show.html');
         },
-        json : function() {
+        json: function () {
           res.json(req.installation);
         }
       });
     },
 
-    new : function(req, res, next) {
+    new: function(req, res, next) {
       res.render('InstallationManager/Installations/new.html');
     },
 
-    create : function create(req, res, next) {
+    create: function (req, res, next) {
       res.format({
-        json : function() {
+        json: function () {
           var installation = new InstallationManager.Installation(req.body);
 
-          installation.save().then(function() {
-            res.json(installation);
-          }).catch(next);
+          installation
+            .save()
+            .then(function () {
+              res.json(installation);
+            })
+            .catch(next);
         }
       });
     },
 
-    edit : function edit(req, res, next) {
+    edit: function (req, res, next) {
       res.format({
-        html : function() {
-          res.render('InstallationManager/Installations/edit.html', { installation : req.installation });
+        html: function () {
+          res.render('InstallationManager/Installations/edit.html');
         },
-        json : function() {
+        json: function () {
           res.json(req.installation);
         }
       });
     },
 
-    update : function update(req, res, next) {
+    update: function (req, res, next) {
       res.format({
-        json : function() {
-          req.installation.updateAttributes(req.body).save().then(function(val) {
-            res.json(req.installation);
-          }).catch(next);
+        json: function() {
+          req.installation
+            .updateAttributes(req.body)
+            .save()
+            .then(function(val) {
+              res.json(req.installation);
+            })
+            .catch(next);
         }
       });
     },
 
-    destroy : function destroy(req, res, next) {
+    destroy: function (req, res, next) {
       res.format({
-        json : function() {
-          req.installation.destroy().then(function() {
-            res.json({deleted: true});
-          }).catch(next);
+        json: function () {
+          req.installation
+            .destroy()
+            .then(function () {
+              res.json({ deleted: true });
+            })
+            .catch(next);
         }
       });
     }
