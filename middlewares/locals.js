c2fe0a5a7dca9e076d62862100c57e87cd53aee9
greduan <me@greduan.com>
2016-03-31 11:55:11 -0600

    Update locals middleware to be async

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -5,31 +5,38 @@ var urlFor = CONFIG.router.helpers;
 var _ = require('lodash');
 
 module.exports = function(req, res, next) {
-  if (CONFIG[CONFIG.environment].sessions !== false && CONFIG.environment !== 'test') {
-    res.locals.csrfToken = req.csrfToken();
-  } else {
-    res.locals.csrfToken = '';
-  }
+  return Promise.resolve()
+    .then(function () {
+      if (CONFIG[CONFIG.environment].sessions !== false && CONFIG.environment !== 'test') {
+        res.locals.csrfToken = req.csrfToken();
+      } else {
+        res.locals.csrfToken = '';
+      }
+    })
+    .then(function () {
+      var role = 'Visitor';
 
-  req.role = 'Visitor';
+      if (req.user) {
+        if (req.url.match(/^(\/InstallationManager)/) !== null) {
+          role = 'Admin';
+        } else {
+          role = 'User';
+        }
+      }
 
-  if (req.user) {
-    if (req.url.match(/^(\/InstallationManager)/) !== null) {
-      req.role = 'Admin';
-    } else {
-      req.role = 'User';
-    }
-  }
+      req.role = role;
+    })
+    .then(function () {
+      if (_.isUndefined(res.locals.helpers)) {
+        res.locals.helpers = {};
+      }
 
-  if (_.isUndefined(res.locals.helpers)) {
-    res.locals.helpers = {};
-  }
+      var helpers = {
+        urlFor: urlFor
+      };
 
-  var helpers = {
-    urlFor: urlFor
-  };
-
-  _.assign(res.locals.helpers, helpers);
-
-  next();
+      _.assign(res.locals.helpers, helpers);
+    })
+    .then(next)
+    .catch(next);
 }
