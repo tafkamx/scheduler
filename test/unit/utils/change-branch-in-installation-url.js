494db3ad967543776db9d6ba9559fbba2ffcd4ff
greduan <me@greduan.com>
2016-04-15 21:28:11 -0500

    Add missing semicolons to test file

--- test/unit/utils/change-branch-in-installation-url.js
+++ test/unit/utils/change-branch-in-installation-url.js
@@ -17,35 +17,35 @@ describe('change-branch-in-installation-url', function () {
     var result = changeBranchInInstallationUrl(urlPort, 'something');
 
     expect(_.isString(result)).to.equal(true);
-    expect(result).to.equal('http://something.installation-one.test-installation.com:3001')
+    expect(result).to.equal('http://something.installation-one.test-installation.com:3001');
   });
 
   it('Should change branch name in URL without port', function () {
     var result = changeBranchInInstallationUrl(url, 'something');
 
     expect(_.isString(result)).to.equal(true);
-    expect(result).to.equal('http://something.installation-one.test-installation.com')
+    expect(result).to.equal('http://something.installation-one.test-installation.com');
   });
 
   it('Should add a branch name to URL with port', function () {
     var result = changeBranchInInstallationUrl(noBranchUrlPort, 'something');
 
     expect(_.isString(result)).to.equal(true);
-    expect(result).to.equal('http://something.installation-one.test-installation.com:3001')
+    expect(result).to.equal('http://something.installation-one.test-installation.com:3001');
   });
 
   it('Should add a branch name to URL without port', function () {
     var result = changeBranchInInstallationUrl(noBranchUrl, 'something');
 
     expect(_.isString(result)).to.equal(true);
-    expect(result).to.equal('http://something.installation-one.test-installation.com')
+    expect(result).to.equal('http://something.installation-one.test-installation.com');
   });
 
   it('Should throw error on invalid URL', function () {
     var urlPort = 'http://default.installation-one.extra.test-installation.com:3001';
 
     var fn = function () {
-      changeBranchInInstallationUrl(urlPort, 'something')
+      changeBranchInInstallationUrl(urlPort, 'something');
     };
 
     expect(fn).to.throw;
