bcce5b8f44cb1a03d2d326d1d8b30198646e0b68
Eduardo Lavaque <me@greduan.com>
2016-05-16 13:26:34 -0500

    Fix PATOS-90, move validation tests from integration to model unit tests (#42)
    * Move M.Branch validation tests to unit tests
    
    * Update domain-container version to 2.5.0
    
    * Move M.User validation tests to unit tests
    
    * Use try catch in M.Branch validation tests
    
    * Organize M.BranchSettings validation tests
    
    * Update check in BranchSettings unit tests
    
    * Organize M.InstallationSettings validation unit tests
    
    * Move IM.User validation tests to unit tests
    
    * Rename {I,}M.ResetPasswordToken validations unit tests
    
    From "Checkit rules" to "Validations".
    
    * Move IM.InstallationsController validation tests to unit tests
    
    * Add private:true to package.json
    
    * Fix npm run test so it returns proper exit codes
    
    * Fix copy-paste errors in M.BranchSettings tests

--- test/unit/models/User.js
+++ test/unit/models/User.js
@@ -1,37 +1,184 @@
 'use strict';
 
 var path = require('path');
-
-var container = UNIT;
+var _ = require('lodash');
 
 describe('M.User', function () {
 
-  before(function (done) {
-    container
-      .create('User', {
-        email: 'user-test@example.com',
-        password: '12345678',
-        role: 'student'
-      })
-      .then(function () {
-        return done();
-      })
-      .catch(done);
+  var container = UNIT;
+
+  after(function () {
+    return Promise.all([
+      container.get('User').query().delete(),
+      // UserInfo deleted automatically by PostgreSQL
+    ]);
   });
 
-  after(function (done) {
-    Promise.all([
-      container.get('User').query().delete(),
-    ])
-      .then(function () {
-        return done();
-      })
-      .catch(done);
+  describe('Validations', function () {
+
+    beforeEach(function () {
+      return Promise.all([
+        container.get('User').query().delete(),
+        // UserInfo deleted automatically by PostgreSQL
+      ]);
+    });
+
+    after(function () {
+      return Promise.all([
+        container.get('User').query().delete(),
+        // UserInfo deleted automatically by PostgreSQL
+      ]);
+    });
+
+    describe('email', function () {
+
+      it('Should fail if the email already exists', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '12345678',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '12345678',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email already exists.')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+      it('Should fail if the email is undefined', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              password: '12345678',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email is required')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+      it('Should fail if the email is empty', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: '1',
+              password: '12345678',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+      it('Should fail if the email is longer than 255 characters', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: _.repeat('a', 256),
+              password: '12345678',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
+              // expect(err.errors.email.message).to.be.equal('The email must not exceed 255 characters long')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+    });
+
+    describe('password', function () {
+
+      it('Should fail if the password is shorter than 8 characters', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '1234567',
+              role: 'student',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.password.message).to.be.equal('The password must be at least 8 characters long')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+    });
+
   });
 
   describe('Relations', function () {
 
     describe('info', function () {
+
+      before(function () {
+        return container
+          .create('User', {
+            email: 'user-test@example.com',
+            password: '12345678',
+            role: 'student'
+          });
+      });
 
       it('Should return a proper UserInfo object', function (doneTest) {
         container.query('User')
