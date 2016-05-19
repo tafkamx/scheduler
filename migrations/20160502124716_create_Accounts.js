303d8a3a41a546dd810402e0ea0233160774752f
Bruce <bruce@websharks-inc.com>
2016-05-19 17:54:15 -0400

    Resolves PATOS-157 and PATOS-151 (#46)
    * Laying down the framework
    
    * Working on migrations and linking functions
    
    * Working on migrations
    
    * Updating validations
    
    * Final changes to Account Model for now. I'll be back.
    
    * Beginning work on Teacher Model... migrations
    
    * Initializing Teacher
    
    * Some small changes. Ready to start debugging
    
    * parse error
    
    * Resolving conflicts
    
    * UserInfo removed... tests passing
    
    * Resolving conflicts
    
    * Resolving conflicts
    
    * Attempting to fix tests
    
    * Fixing some 500 errors
    
    * Making Accounts work with ACL
    
    * Removing InstallationManager.UserInfo
    
    * Finishing up AccountsController (edit does nothing)
    
    * Attempting to make Controller work within routeMapper
    
    * Controller now a valid resource. 1 test passing
    
    * First three integration tests
    
    * All but modification controllers done
    
    * Adding checkit as package
    
    * Fixing migrations
    
    * Fixing some more conflict errors
    
    * Fixing issues brought up in PR

---
+++ migrations/20160502124716_create_Accounts.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('Accounts', function (t) {
+    t.uuid('id').primary();
+    t.uuid('user_id') // This can be NULL for Accounts that do not have Authentication
+      .references('id')
+      .inTable('Users')
+      .onDelete('CASCADE');
+    t.string('branch_name', 255).notNullable();
+    t.string('type', 25).notNullable();
+    t.string('first_name', 125);
+    t.string('last_name', 125);
+    t.date('dob');
+    t.string('address_line_1', 255);
+    t.string('address_line_2', 255);
+    t.string('city', 255);
+    t.string('state', 255);
+    t.string('country', 48); // Longest country name in the world is 45 Letters "of Great Britain and Northern Ireland"
+    t.string('postal_code', 48);
+    t.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('Accounts');
+};
---
+++ models/M/Teacher.js
@@ -0,0 +1,63 @@
+/**
+ *
+ */
+var Teacher = Class(M, 'Teacher').inherits(Krypton.Model)({
+  tableName: 'Teachers',
+
+  validations: {
+    id: ['natural'], // This is using int value instead of UUID, because it's not necessary.
+    accountId: ['uuid', 'required'],
+    active: ['boolean'],
+    baseWage: ['numeric']
+  },
+
+  attributes: [
+    'id',
+    'accountId',
+    'active',
+    'bio',
+    'baseWage',
+    'accountNumber',
+    'voidCheck',
+    'createdAt',
+    'updatedAt'
+  ],
+
+  prototype: {
+    init: function(config) {
+      DynamicModel.prototype.init.call(this, config);
+      var instance = this;
+
+      instance.on('beforeCreate', function(next) {
+        if(typeof instance.id !== 'number') delete instance.id; // This model uses incremental integer IDs
+        instance.active = !!instance.active; // Force typing
+        next();
+      });
+    },
+
+    getAvailability: function() {
+
+    },
+
+    isAvailable: function(days, hours) {
+
+    },
+
+    updateAvailability: function(days, hours) {
+
+    }
+  }
+
+  /*
+  relations: {
+    availability: {
+      type: 'HasOne',
+      relatedModel: TeacherAvailability,
+      ownerCol: 'account_id',
+      relatedCol: 'id'
+    }
+  }
+  */
+});
+
+module.exports = Teacher;
