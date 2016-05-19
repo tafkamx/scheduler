df79b0291f8c027447d8b86322154aa2fdc97616
Bruce <bruce@myinbox.ws>
2016-05-19 13:01:41 -0400

    Fixing formatting on Users tests

--- test/unit/models/User.js
+++ test/unit/models/User.js
@@ -29,133 +29,132 @@ describe('M.User', function () {
       .catch(done);
   });
 
+  describe('Validations', function() {
+    describe('email', function () {
+      it('Should fail if the email already exists', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '12345678',
+            });
+          })
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '12345678',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email already exists.')
+            } catch (err) {
+              return done(err);
+            }
 
-  describe('email', function () {
-    it('Should fail if the email already exists', function (done) {
-      return Promise.resolve()
-        .then(function () {
-          return container.create('User', {
-            email: 'user-test@example.com',
-            password: '12345678',
+            done();
           });
-        })
-        .then(function () {
-          return container.create('User', {
-            email: 'user-test@example.com',
-            password: '12345678',
-          });
-        })
-        .then(function () {
-          expect.fail('should have rejected');
-        })
-        .catch(function (err) {
-          try {
-            expect(err.message).to.be.equal('1 invalid values');
-            expect(err.errors.email.message).to.be.equal('The email already exists.')
-          } catch (err) {
-            return done(err);
-          }
+      });
 
-          done();
-        });
+      it('Should fail if the email is undefined', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              password: '12345678',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email is required')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+      it('Should fail if the email is empty', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: '1',
+              password: '12345678',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
+      it('Should fail if the email is longer than 255 characters', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: _.repeat('a', 256),
+              password: '12345678',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
+              // expect(err.errors.email.message).to.be.equal('The email must not exceed 255 characters long')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
+          });
+      });
+
     });
 
-    it('Should fail if the email is undefined', function (done) {
-      return Promise.resolve()
-        .then(function () {
-          return container.create('User', {
-            password: '12345678',
+    describe('password', function () {
+
+      it('Should fail if the password is shorter than 8 characters', function (done) {
+        return Promise.resolve()
+          .then(function () {
+            return container.create('User', {
+              email: 'user-test@example.com',
+              password: '1234567',
+            });
+          })
+          .then(function () {
+            expect.fail('should have rejected');
+          })
+          .catch(function (err) {
+            try {
+              //expect(err.message).to.be.equal('1 invalid values');
+              expect(err.errors.password.message).to.be.equal('The password must be at least 8 characters long')
+            } catch (err) {
+              return done(err);
+            }
+
+            done();
           });
-        })
-        .then(function () {
-          expect.fail('should have rejected');
-        })
-        .catch(function (err) {
-          try {
-            expect(err.message).to.be.equal('1 invalid values');
-            expect(err.errors.email.message).to.be.equal('The email is required')
-          } catch (err) {
-            return done(err);
-          }
-
-          done();
-        });
+      });
     });
-
-    it('Should fail if the email is empty', function (done) {
-      return Promise.resolve()
-        .then(function () {
-          return container.create('User', {
-            email: '1',
-            password: '12345678',
-          });
-        })
-        .then(function () {
-          expect.fail('should have rejected');
-        })
-        .catch(function (err) {
-          try {
-            expect(err.message).to.be.equal('1 invalid values');
-            expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
-          } catch (err) {
-            return done(err);
-          }
-
-          done();
-        });
-    });
-
-    it('Should fail if the email is longer than 255 characters', function (done) {
-      return Promise.resolve()
-        .then(function () {
-          return container.create('User', {
-            email: _.repeat('a', 256),
-            password: '12345678',
-          });
-        })
-        .then(function () {
-          expect.fail('should have rejected');
-        })
-        .catch(function (err) {
-          try {
-            expect(err.message).to.be.equal('1 invalid values');
-            expect(err.errors.email.message).to.be.equal('The email must be a valid email address')
-            // expect(err.errors.email.message).to.be.equal('The email must not exceed 255 characters long')
-          } catch (err) {
-            return done(err);
-          }
-
-          done();
-        });
-    });
-
   });
-
-  describe('password', function () {
-
-    it('Should fail if the password is shorter than 8 characters', function (done) {
-      return Promise.resolve()
-        .then(function () {
-          return container.create('User', {
-            email: 'user-test@example.com',
-            password: '1234567',
-          });
-        })
-        .then(function () {
-          expect.fail('should have rejected');
-        })
-        .catch(function (err) {
-          try {
-            //expect(err.message).to.be.equal('1 invalid values');
-            expect(err.errors.password.message).to.be.equal('The password must be at least 8 characters long')
-          } catch (err) {
-            return done(err);
-          }
-
-          done();
-        });
-    });
-
-  });
-
 });
