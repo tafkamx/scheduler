00c1ab32ca54e6791a86e3696ca9ba6b7253b6c2
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-17 12:38:54 -0500

    Halt the application on any error during boot; fix

--- bin/server.js
+++ bin/server.js
@@ -5,25 +5,31 @@ var nodemailer = require('nodemailer');
 var mailgun = require('nodemailer-mailgun-transport');
 var glob = require('glob');
 
-var neonode = require(path.join(process.cwd(), 'lib', 'core'));
+try {
+  var neonode = require(path.join(process.cwd(), 'lib', 'core'));
 
-// Load model relations
-glob.sync('lib/model-relations/**/*.js').forEach(function (file) {
-  require(path.join(process.cwd(), file));
-});
+  // Load model relations
+  glob.sync('lib/model-relations/**/*.js').forEach(function (file) {
+    require(path.join(process.cwd(), file));
+  });
 
-var transport;
+  var transport;
 
-if (CONFIG.environment === 'test') {
-  transport = require('nodemailer-stub-transport')();
-} else {
-  transport = mailgun(CONFIG[CONFIG.environment].mailer.mailgun);
+  if (CONFIG.environment === 'test') {
+    transport = require('nodemailer-stub-transport')();
+  } else {
+    transport = mailgun(CONFIG[CONFIG.environment].mailer.mailgun);
+  }
+
+  BaseMailer.transport(nodemailer.createTransport(transport));
+
+  // Start
+
+  neonode._serverStart();
+} catch (e) {
+  logger.error(e.stack);
+  process.exit(1);
 }
-
-BaseMailer.transport(nodemailer.createTransport(transport));
-
-// Start
-neonode._serverStart();
 
 // When there's an unhandled rejection just log the error and stop the process
 process.on('unhandledRejection', function (err, promise) {
