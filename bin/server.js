31e126c2aaaf5432888cfdb5cf93a7bc29669aea
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-17 13:01:17 -0500

    Fixed error handling; remove try-catch

--- bin/server.js
+++ bin/server.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
+process.on('uncaughtException', function (err) {
+  logger.error(err.stack || err);
+  console.log(err);
+  process.exit(1);
+});
+
 var path = require('path');
 var nodemailer = require('nodemailer');
 var mailgun = require('nodemailer-mailgun-transport');
 var glob = require('glob');
 
-try {
-  var neonode = require(path.join(process.cwd(), 'lib', 'core'));
+var neonode = require(path.join(process.cwd(), 'lib', 'core'));
 
-  // Load model relations
-  glob.sync('lib/model-relations/**/*.js').forEach(function (file) {
-    require(path.join(process.cwd(), file));
-  });
+// Load model relations
+glob.sync('lib/model-relations/**/*.js').forEach(function (file) {
+  require(path.join(process.cwd(), file));
+});
 
-  var transport;
+var transport;
 
-  if (CONFIG.environment === 'test') {
-    transport = require('nodemailer-stub-transport')();
-  } else {
-    transport = mailgun(CONFIG[CONFIG.environment].mailer.mailgun);
-  }
-
-  BaseMailer.transport(nodemailer.createTransport(transport));
-
-  // Start
-
-  neonode._serverStart();
-} catch (e) {
-  logger.error(e.stack);
-  process.exit(1);
+if (CONFIG.environment === 'test') {
+  transport = require('nodemailer-stub-transport')();
+} else {
+  transport = mailgun(CONFIG[CONFIG.environment].mailer.mailgun);
 }
+
+BaseMailer.transport(nodemailer.createTransport(transport));
+
+// Start
+neonode._serverStart();
 
 // When there's an unhandled rejection just log the error and stop the process
 process.on('unhandledRejection', function (err, promise) {
